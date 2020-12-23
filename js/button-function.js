5a375afc76aab4cd6f5aeee47798138a86a72db3
madrid95 <madriddajsinani@gmail.com>
2020-12-23 17:22:54 +0100

    updated

--- js/button-function.js
+++ js/button-function.js
@@ -8,6 +8,14 @@ function toggleLikeButton(el) {
         el.classList.add("mdi-heart-outline");
         el.classList.remove("mdi-heart")
     }
+}
 
-    
+function ratingListener (el) {
+    if (el.classList.contains("mdi-star-outline")) {
+        el.classList.remove("mdi-star-outline")
+        el.classList.add("mdi-star");
+    } else if (el.classList.contains("mdi-star")) {
+        el.classList.add("mdi-star-outline");
+        el.classList.remove("mdi-star")
+    }
 }
